d74ceb7d757b77cffa00d19d20691c9911cb2fe6
Tom <tom@itsounds.de>
2019-04-22 16:05:09 +0200

    Initial Commit. Framework / Header und File Struktur. Deklarationen, Funktionsnamen, typedef TDate & TTime

---
+++ ueb01/datastructure.h
@@ -0,0 +1,23 @@
+//
+//  datastructure.h
+//  ueb01X
+//
+//  Created by Tom Mertens on 20.04.19.
+//  Copyright © 2019 Tom Mertens. All rights reserved.
+//
+
+#ifndef datastructure_h
+#define datastructure_h
+typedef struct
+{   int Day;
+    int Month;
+    int Year;
+} TDate[50];
+
+typedef struct
+{   int Hour;
+    int Minute;
+    int Second;
+} TTime[50];
+
+#endif /* datastructure_h */
---
+++ ueb01/datetime.c
@@ -0,0 +1,144 @@
+//
+//  datetime.c
+//  ueb01X
+//
+//  Created by Tom Mertens on 20.04.19.
+//  Copyright © 2019 Tom Mertens. All rights reserved.
+//
+
+#include "datetime.h"
+#include "tools.h"
+#include "datastructure.h"
+/***************************************************************************
+ Funktion:  isLeapYear
+ Parameter: Jahreszahl als int
+ Ergebnis:  Wahrheitswert
+ Beschreib: Wahr = Jahr ist ein Schaltjahr
+ ***************************************************************************/
+int isLeapYear(int year)
+{
+    
+}
+/***************************************************************************
+ Funktion:  isDateValid
+ Parameter: struct TDate
+ Ergebnis:  Wahrheitswert als int
+ Beschreib: Angeben ob das uebergebene Datum gueltig ist
+ ***************************************************************************/
+int isDateValid(TDate Date)
+{
+    
+}
+/***************************************************************************
+ Funktion:  isTimeValid
+ Parameter: struct TTime
+ Ergebnis:  Wahrheitswert als int
+ Beschreib: Angeben ob die uebergebene Uhrzeit gueltig ist
+ ***************************************************************************/
+int isTimeValid(TTime Time)
+{
+    
+}
+/***************************************************************************
+ Funktion:  getDateFromString
+ Parameter: *char Input, *TDate
+ Ergebnis:  Wahrheitswert als int bei erfolgreichem Kopieren
+ Beschreib: Liest ein Datum aus einer Zeichenkette und kopiert die in Struct, ruft IsDateValid auf
+ ***************************************************************************/
+int getDateFromString(char *Input, TDate Date)
+{
+    
+}
+/***************************************************************************
+ Funktion:  getTimeFromString
+ Parameter: *char Input, *TTime
+ Ergebnis:  Wahrheitswert als int bei erfolgreichem Kopieren
+ Beschreib: Liest eine Zeit aus einer Zeichenkette und kopiert die in Struct, ruft IsTimeValid auf
+ ***************************************************************************/
+int getTimeFromString(*char Input, TTime Time)
+{
+    
+}
+/***************************************************************************
+ Funktion:
+ Parameter:
+ Ergebnis:
+ Beschreib:
+ ***************************************************************************/
+
+/***************************************************************************
+ Funktion:
+ Parameter:
+ Ergebnis:
+ Beschreib:
+ ***************************************************************************/
+
+/***************************************************************************
+ Funktion:
+ Parameter:
+ Ergebnis:
+ Beschreib:
+ ***************************************************************************/
+
+/***************************************************************************
+ Funktion:
+ Parameter:
+ Ergebnis:
+ Beschreib:
+ ***************************************************************************/
+
+/***************************************************************************
+ Funktion:
+ Parameter:
+ Ergebnis:
+ Beschreib:
+ ***************************************************************************/
+
+/***************************************************************************
+ Funktion:
+ Parameter:
+ Ergebnis:
+ Beschreib:
+ ***************************************************************************/
+
+/***************************************************************************
+ Funktion:
+ Parameter:
+ Ergebnis:
+ Beschreib:
+ ***************************************************************************/
+
+/***************************************************************************
+ Funktion:
+ Parameter:
+ Ergebnis:
+ Beschreib:
+ ***************************************************************************/
+
+/***************************************************************************
+ Funktion:
+ Parameter:
+ Ergebnis:
+ Beschreib:
+ ***************************************************************************/
+
+/***************************************************************************
+ Funktion:
+ Parameter:
+ Ergebnis:
+ Beschreib:
+ ***************************************************************************/
+
+/***************************************************************************
+ Funktion:
+ Parameter:
+ Ergebnis:
+ Beschreib:
+ ***************************************************************************/
+
+/***************************************************************************
+ Funktion:
+ Parameter:
+ Ergebnis:
+ Beschreib:
+ ***************************************************************************/
