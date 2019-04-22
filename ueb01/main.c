985072d28640a59ebfd59a5c0623d479745437d8
Tom <tom@itsounds.de>
2019-04-22 13:57:51 +0200

    Initial Commit

---
+++ ueb01/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ueb01
+//
+//  Created by Tom Mertens on 22.04.19.
+//  Copyright © 2019 Tom Mertens. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
