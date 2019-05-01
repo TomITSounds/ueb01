7707601f9ec4146df1eabe56565330c6f5cfe74b
freddiefreund <49882708+freddiefreund@users.noreply.github.com>
2019-05-01 22:32:17 +0200

    ueb01 Projekt fertig
    jo meine(und Junas) version

---
+++ Ueb01Proj/tools.c
@@ -0,0 +1,84 @@
+//
+//  tools.c
+//  ueb01X
+//
+//  Created by Tom Mertens on 20.04.19.
+//  Copyright © 2019 Tom Mertens. All rights reserved.
+//
+
+#include <stdio.h>
+#include "tools.h"
+#include <stdlib.h>
+
+/**********************************************************
+ * Funktion: clearBuffer
+ * Beschreibung: Leert den Tasturpuffer
+ * Parameter: Character
+ * Ergebnis:
+ **********************************************************/
+void clearBuffer()
+{
+    char dummy;
+    do
+    {  scanf("%c", &dummy);
+    }  while (dummy != '\n');
+}
+
+/**********************************************************
+ * Funktion: askAgain
+ * Beschreibung: Fragt den Benutzer ob er dass Programm nochmal ausfuehren moecht
+ Parameter: Zeile und Spalte der Frage
+ *Ergebnis: Wahrheitswert
+ **********************************************************
+
+int askAgain(int Row, int Col)
+{  int ask = 0;
+    char ans = 0;
+
+    POSITION(Row, Col);
+    printf("Wolle sie nochmal? j/n\n");
+    do
+    {   POSITION (Row+1, Col);
+        CLEAR_LINE;
+        scanf("%c", &ans);
+        if (ans != '\n')
+            clearBuffer();
+        switch (ans)
+        {   case 'j':
+            case 'J':   ask = 1; break;
+            case 'n':
+            case 'N':   ask = 0; break;
+            default:    printf("Ungueltige Eingabe. Wollen sie nochmal? j/n\n");
+        }
+    }  while ( (ans != 'j') && (ans != 'J') && (ans != 'N') && (ans !='n') );
+    return ask;
+}
+
+void stop()
+{  char c;
+    do
+    {  scanf("%c", &c);
+    }  while (c != '\n');
+}
+
+/**********************************************************
+ * Funktion: waitForEnter
+ * Beschreibung:
+ * Parameter:
+ * Ergebnis:
+ **********************************************************/
+void waitForEnter()
+{
+
+}
+
+/**********************************************************
+ * Funktion:        ClearScreen
+ * Beschreibung:    Loescht den Bildschirm
+ Parameter:
+ *Ergebnis:
+ **********************************************************/
+void clearScreen()
+{   system("clear");
+    system("cls");
+}
